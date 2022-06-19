e427d0fb66c96ca5f63436d58112e976e26cba51
Pramod Kumar S Sarvi <pramodkumarssarvi@gmail.com>
2022-06-19 21:02:34 +0530

    Fix cors2

--- app.py
+++ app.py
@@ -10,11 +10,12 @@ origins = [
     "https://localhost.tiangolo.com",
     "http://localhost",
     "http://localhost:8080",
+    "http://localhost:3000",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins='*',
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
